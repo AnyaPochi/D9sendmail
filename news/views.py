d2765faf6b8a0e12f8a80e39c4ddbae71483a0f6
AnyaPochi <podchinenova.as@gmail.com>
2024-02-20 15:09:00 +0500

    checkplease

---
+++ news/views.py
@@ -0,0 +1,108 @@
+
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView,DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Post
+from .forms import PostForm
+from datetime import datetime
+from .filters import PostFilter
+from django.contrib.auth.mixins import PermissionRequiredMixin
+class PostsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    # model = Post
+    # # Поле, которое будет использоваться для сортировки объектов
+    # ordering = 'title'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    queryset = Post.objects.order_by('-time_in')
+    template_name = 'posts.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    # def get_queryset(self):
+    #     # Получаем обычный запрос
+    #     queryset = super().get_queryset()
+    #     # Используем наш класс фильтрации.
+    #     # self.request.GET содержит объект QueryDict, который мы рассматривали
+    #     # в этом юните ранее.
+    #     # Сохраняем нашу фильтрацию в объекте класса,
+    #     # чтобы потом добавить в контекст и использовать в шаблоне.
+    #     self.filterset = PostFilter(self.request.GET, queryset)
+    #     # Возвращаем из функции отфильтрованный список товаров
+    #     return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    queryset = Post.objects.order_by('-time_in')
+    template_name = 'post.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+class PostsSearch(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset
+
+        return context
+# Добавляем новое представление для создания товаров.
+class PostCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if self.request.path == '/news/articles/create/':
+            post.type = 'Статья'
+        post.save()
+        return super().form_valid(form)
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
+
+class AddPost(PermissionRequiredMixin, PostCreate):
+    permission_required = ('news.add_post',)
+
+class ChangePost(PermissionRequiredMixin, PostUpdate):
+    permission_required = ('news.change_post')
